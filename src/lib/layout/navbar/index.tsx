af8fee145930b978b5831b8503dc1f8a90c2b596
Gabriel <gabriel.cafe@ufrpe.br>
2024-02-02 17:12:25 -0300

    alteração com filtro

--- src/lib/layout/navbar/index.tsx
+++ src/lib/layout/navbar/index.tsx
@@ -1,15 +1,32 @@
-import {
-  Editable,
-  EditableInput,
-  EditableTextarea,
-  EditablePreview,
-} from '@chakra-ui/react';
+import { SearchIcon } from '@chakra-ui/icons';
+import { Input, InputGroup, InputRightAddon, Stack, IconButton } from '@chakra-ui/react';
+import { useState } from 'react';
 
-export const Navbar = () => {
+export const Navbar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = () => {
+    // Chama a função de pesquisa passando o termo de pesquisa
+    onSearch(searchTerm);
+  };
+
   return (
-    <Editable defaultValue="Take some chakra">
-      <EditablePreview />
-      <EditableTextarea />
-    </Editable>
+    <Stack width="100%" margin={8}>
+      <InputGroup size="lg">
+        <Input
+          placeholder="Pesquisa"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <InputRightAddon>
+          <IconButton
+            onClick={handleSearch}
+            colorScheme="blue"
+            aria-label="Search database"
+            icon={<SearchIcon />}
+          />
+        </InputRightAddon>
+      </InputGroup>
+    </Stack>
   );
 };
--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,9 +1,15 @@
-import { Flex, Wrap, WrapItem } from '@chakra-ui/react';
+import {
+  Button,
+  Flex,
+  Input,
+  InputGroup,
+  Wrap,
+  WrapItem,
+} from '@chakra-ui/react';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 import { PokeCard } from '~/lib/components/Card';
-import { Layout } from '~/lib/layout';
 
 interface IPokemon {
   name: string;
@@ -12,12 +18,14 @@ interface IPokemon {
 
 export const Home = () => {
   const [pokemons, setPokemons] = useState<IPokemon[]>([]);
+  const [countPokemons, setCountPokemons] = useState(20);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     const fetchPokemons = async () => {
       try {
         const response = await axios.get(
-          'https://pokeapi.co/api/v2/pokemon/?limit=20'
+          `https://pokeapi.co/api/v2/pokemon/?limit=${countPokemons}`
         );
         setPokemons(response.data.results);
       } catch (error) {
@@ -26,20 +34,31 @@ export const Home = () => {
     };
 
     fetchPokemons();
-  }, []);
+  }, [countPokemons]);
+
+  //console.log(pokemons);
 
   return (
-    <Layout>
-      <Flex justify="flex-start" align="flex-start" w="100vw" h="100vh">
-        <Wrap spacing={4}>
-          {/* Displaying list of Pokémon */}
-          {pokemons.map((pokemon: IPokemon) => (
+    <Flex justify="flex-start" align="flex-start" w="100vw" h="100vh">
+      <InputGroup size="lg">
+        <Input
+          placeholder="Pesquisa"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </InputGroup>
+      <Wrap spacing={4}>
+        {pokemons
+          .filter((pokemon: IPokemon) =>
+            pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .map((pokemon: IPokemon) => (
             <WrapItem key={pokemon.name}>
               <PokeCard name={pokemon.name} url={pokemon.url} />
             </WrapItem>
           ))}
-        </Wrap>
-      </Flex>
-    </Layout>
+      </Wrap>
+      <Button onClick={() => setCountPokemons(countPokemons + 5)}>+++</Button>
+    </Flex>
   );
 };
